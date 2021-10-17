bd5335fe7c5b5a9274b381fd4cd099ae81664de6
Brian-Kipyegon <kipyegon.c.brian@gmail.com>
2021-10-17 21:40:40 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,45 @@
+package com.example.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentByEmail = studentRepository.
+                findStudentByEmail(student.getEmail());
+        if (studentByEmail.isPresent()) {
+            throw new IllegalStateException("Email is already taken");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        if (studentRepository.existsById(studentId)) {
+            studentRepository.deleteById(studentId);
+        } else {
+            throw new IllegalStateException("student doesn't exist");
+        }
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+    }
+}
